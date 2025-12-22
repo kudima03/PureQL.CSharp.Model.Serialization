594809ad92816e064b81660c076fdaaad49b1e58
Dmitry Kurochkin <kudima03@gmail.com>
2025-12-22 12:43:49 +0300

    Implemented BooleanFieldConverterTests

---
+++ src/Tests/PureQL.CSharp.Model.Serialization.Tests/Fields/BooleanFieldConverterTests.cs
@@ -0,0 +1,89 @@
+using System.Text.Json;
+using PureQL.CSharp.Model.Fields;
+using PureQL.CSharp.Model.Serialization.Fields;
+using PureQL.CSharp.Model.Serialization.Types;
+using PureQL.CSharp.Model.Types;
+
+namespace PureQL.CSharp.Model.Serialization.Tests.Fields;
+
+public sealed record BooleanFieldConverterTests
+{
+    private readonly JsonSerializerOptions _options = new JsonSerializerOptions()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        PropertyNameCaseInsensitive = true,
+        Converters = { new BooleanFieldConverter(), new TypeConverter<BooleanType>() },
+    };
+
+    [Fact]
+    public void ReadEntity()
+    {
+        const string expected = "test";
+
+        const string input = /*lang=json,strict*/
+            $$"""{"type": {"name":"boolean"},"entity": "{{expected}}","field": "test"}""";
+
+        BooleanField scalar = JsonSerializer.Deserialize<BooleanField>(input, _options)!;
+
+        Assert.Equal(expected, scalar.Entity);
+    }
+
+    [Fact]
+    public void ReadField()
+    {
+        const string expected = "test";
+
+        const string input = /*lang=json,strict*/
+            $$"""{"type": {"name":"boolean"},"entity": "test","field": "{{expected}}"}""";
+
+        BooleanField scalar = JsonSerializer.Deserialize<BooleanField>(input, _options)!;
+
+        Assert.Equal(expected, scalar.Field);
+    }
+
+    [Fact]
+    public void Write()
+    {
+        const string expected =
+            /*lang=json,strict*/
+            """
+                {"entity":"auiheyrdsnf","field":"jinaudferv","type":{"name":"boolean"}}
+                """;
+
+        string output = JsonSerializer.Serialize(
+            new BooleanField("auiheyrdsnf", "jinaudferv"),
+            _options
+        );
+
+        Assert.Equal(expected, output);
+    }
+
+    [Theory]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"datetime"},"entity": "auiheyrdsnf","field": "jinaudferv"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"date"},"entity": "auiheyrdsnf","field": "jinaudferv"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"null"},"entity": "auiheyrdsnf","field": "jinaudferv"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"number"},"entity": "auiheyrdsnf","field": "jinaudferv"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"string"},"entity": "auiheyrdsnf","field": "jinaudferv"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"time"},"entity": "auiheyrdsnf","field": "jinaudferv"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"uuid"},"entity": "auiheyrdsnf","field": "jinaudferv"}"""
+    )]
+    public void ThrowsExceptionOnWrongType(string input)
+    {
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<BooleanField>(input, _options)
+        );
+    }
+}
