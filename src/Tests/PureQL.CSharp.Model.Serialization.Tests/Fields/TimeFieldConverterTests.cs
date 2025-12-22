2a3809afd87bd3351be0f8e45ac5f06b7548463a
Dmitry Kurochkin <kudima03@gmail.com>
2025-12-22 15:25:47 +0300

    implemented TimeFieldConverterTests

---
+++ src/Tests/PureQL.CSharp.Model.Serialization.Tests/Fields/TimeFieldConverterTests.cs
@@ -0,0 +1,87 @@
+using System.Text.Json;
+using PureQL.CSharp.Model.Fields;
+using PureQL.CSharp.Model.Serialization.Fields;
+using PureQL.CSharp.Model.Serialization.Types;
+using PureQL.CSharp.Model.Types;
+
+namespace PureQL.CSharp.Model.Serialization.Tests.Fields;
+
+public sealed record TimeFieldConverterTests
+{
+    private readonly JsonSerializerOptions _options = new JsonSerializerOptions()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        PropertyNameCaseInsensitive = true,
+        Converters = { new TimeFieldConverter(), new TypeConverter<TimeType>() },
+    };
+
+    [Fact]
+    public void ReadEntity()
+    {
+        const string expected = "test";
+
+        const string input = /*lang=json,strict*/
+            $$"""{"type": {"name":"time"},"entity": "{{expected}}","field": "test"}""";
+
+        TimeField field = JsonSerializer.Deserialize<TimeField>(input, _options)!;
+
+        Assert.Equal(expected, field.Entity);
+    }
+
+    [Fact]
+    public void ReadField()
+    {
+        const string expected = "test";
+
+        const string input = /*lang=json,strict*/
+            $$"""{"type": {"name":"time"},"entity": "test","field": "{{expected}}"}""";
+
+        TimeField field = JsonSerializer.Deserialize<TimeField>(input, _options)!;
+
+        Assert.Equal(expected, field.Field);
+    }
+
+    [Fact]
+    public void Write()
+    {
+        const string expected =
+            /*lang=json,strict*/
+            """{"entity":"auiheyrdsnf","field":"jinaudferv","type":{"name":"time"}}""";
+
+        string output = JsonSerializer.Serialize(
+            new TimeField("auiheyrdsnf", "jinaudferv"),
+            _options
+        );
+
+        Assert.Equal(expected, output);
+    }
+
+    [Theory]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"date"},"entity": "auiheyrdsnf","field": "jinaudferv"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"boolean"},"entity": "auiheyrdsnf","field": "jinaudferv"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"null"},"entity": "auiheyrdsnf","field": "jinaudferv"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"datetime"},"entity": "auiheyrdsnf","field": "jinaudferv"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"number"},"entity": "auiheyrdsnf","field": "jinaudferv"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"string"},"entity": "auiheyrdsnf","field": "jinaudferv"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"uuid"},"entity": "auiheyrdsnf","field": "jinaudferv"}"""
+    )]
+    public void ThrowsExceptionOnWrongType(string input)
+    {
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<TimeField>(input, _options)
+        );
+    }
+}
