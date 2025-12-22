4374fa6703702d91ab72701e49e211462d24de73
Dmitry Kurochkin <kudima03@gmail.com>
2025-12-22 10:06:29 +0300

    Implemented DateTimeScalarConverter

---
+++ src/PureQL.CSharp.Model.Serialization/Scalars/DateTimeScalarConverter.cs
@@ -0,0 +1,47 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using PureQL.CSharp.Model.Scalars;
+using PureQL.CSharp.Model.Types;
+
+namespace PureQL.CSharp.Model.Serialization.Scalars;
+
+internal sealed record DateTimeScalarJsonModel
+{
+    public DateTimeScalarJsonModel(IDateTimeScalar scalar)
+        : this(new DateTimeType(), scalar.Value) { }
+
+    [JsonConstructor]
+    public DateTimeScalarJsonModel(DateTimeType type, DateTime value)
+    {
+        Type = type;
+        Value = value;
+    }
+
+    public DateTimeType Type { get; }
+
+    public DateTime Value { get; }
+}
+
+public sealed class DateTimeScalarConverter : JsonConverter<IDateTimeScalar>
+{
+    public override IDateTimeScalar? Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        DateTimeScalarJsonModel scalar =
+            JsonSerializer.Deserialize<DateTimeScalarJsonModel>(ref reader, options)!;
+
+        return new DateTimeScalar(scalar.Value);
+    }
+
+    public override void Write(
+        Utf8JsonWriter writer,
+        IDateTimeScalar value,
+        JsonSerializerOptions options
+    )
+    {
+        JsonSerializer.Serialize(writer, new DateTimeScalarJsonModel(value), options);
+    }
+}
