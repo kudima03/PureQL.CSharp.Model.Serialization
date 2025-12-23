15d009ded27fd4c2ae594b266a63f8db295e7217
Dmitry Kurochkin <kudima03@gmail.com>
2025-12-23 10:13:30 +0300

    Implemented TimeParameterConverterTests

---
+++ src/Tests/PureQL.CSharp.Model.Serialization.Tests/Parameters/TimeParameterConverterTests.cs
@@ -0,0 +1,76 @@
+using System.Text.Json;
+using PureQL.CSharp.Model.Parameters;
+using PureQL.CSharp.Model.Serialization.Parameters;
+using PureQL.CSharp.Model.Serialization.Types;
+using PureQL.CSharp.Model.Types;
+
+namespace PureQL.CSharp.Model.Serialization.Tests.Parameters;
+
+public sealed record TimeParameterConverterTests
+{
+    private readonly JsonSerializerOptions _options = new JsonSerializerOptions()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        PropertyNameCaseInsensitive = true,
+        Converters = { new TimeParameterConverter(), new TypeConverter<TimeType>() },
+    };
+
+    [Fact]
+    public void ReadName()
+    {
+        const string expected = "iurhgndfjsb";
+
+        const string input = /*lang=json,strict*/
+            $$"""{"type": {"name":"time"},"name": "{{expected}}"}""";
+
+        TimeParameter parameter = JsonSerializer.Deserialize<TimeParameter>(
+            input,
+            _options
+        )!;
+
+        Assert.Equal(expected, parameter.Name);
+    }
+
+    [Fact]
+    public void Write()
+    {
+        const string expected = /*lang=json,strict*/
+            """{"name":"auiheyrdsnf","type":{"name":"time"}}""";
+
+        string output = JsonSerializer.Serialize(
+            new TimeParameter("auiheyrdsnf"),
+            _options
+        );
+
+        Assert.Equal(expected, output);
+    }
+
+    [Theory]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"date"},"name": "auiheyrdsnf"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"boolean"},"name": "auiheyrdsnf"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"datetime"},"name": "auiheyrdsnf"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"null"},"name": "auiheyrdsnf"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"number"},"name": "auiheyrdsnf"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"string"},"name": "auiheyrdsnf"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"uuid"},"name": "auiheyrdsnf"}"""
+    )]
+    public void ThrowsExceptionOnWrongType(string input)
+    {
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<TimeParameter>(input, _options)
+        );
+    }
+}
