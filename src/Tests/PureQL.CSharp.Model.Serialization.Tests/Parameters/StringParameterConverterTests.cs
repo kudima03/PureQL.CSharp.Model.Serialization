bc128b14351f87ca09a5e8b6afc8a219065e1f36
Dmitry Kurochkin <kudima03@gmail.com>
2025-12-24 11:19:55 +0300

    Added tests for bad format

--- src/Tests/PureQL.CSharp.Model.Serialization.Tests/Parameters/StringParameterConverterTests.cs
+++ src/Tests/PureQL.CSharp.Model.Serialization.Tests/Parameters/StringParameterConverterTests.cs
@@ -56,6 +56,18 @@ public sealed record StringParameterConverterTests
     }
 
     [Theory]
+    [InlineData("{}")]
+    [InlineData("{asdasdasd}")]
+    [InlineData("""{"asdasd":   }""")]
+    [InlineData(""" """)]
+    public void ThrowsExceptionOnBadFormat(string input)
+    {
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<StringParameter>(input, _options)
+        );
+    }
+
+    [Theory]
     [InlineData( /*lang=json,strict*/
         """{"type":{"name":"date"},"name": "auiheyrdsnf"}"""
     )]
