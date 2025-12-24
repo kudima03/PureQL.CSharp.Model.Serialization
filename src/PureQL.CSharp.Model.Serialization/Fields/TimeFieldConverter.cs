39b2a0d93ce10580d8b6a7ae705c3370f5f3fae3
Dmitry Kurochkin <kudima03@gmail.com>
2025-12-24 09:58:09 +0300

    Added null checks

--- src/PureQL.CSharp.Model.Serialization/Fields/TimeFieldConverter.cs
+++ src/PureQL.CSharp.Model.Serialization/Fields/TimeFieldConverter.cs
@@ -13,9 +13,9 @@ internal sealed record TimeFieldJsonModel
     [JsonConstructor]
     public TimeFieldJsonModel(string entity, string field, TimeType type)
     {
-        Entity = entity;
-        Field = field;
-        Type = type;
+        Entity = entity ?? throw new JsonException();
+        Field = field ?? throw new JsonException();
+        Type = type ?? throw new JsonException();
     }
 
     public string Entity { get; }
