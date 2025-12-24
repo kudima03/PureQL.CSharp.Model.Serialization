6193f783ee4ff406ed5b2499a6c3981b2fab0edd
Dmitry Kurochkin <kudima03@gmail.com>
2025-12-24 14:33:56 +0300

    Implemented DateReturningConverterTests

---
+++ src/Tests/PureQL.CSharp.Model.Serialization.Tests/Returnings/DateReturningConverterTests.cs
@@ -0,0 +1,240 @@
+using System.Text.Json;
+using PureQL.CSharp.Model.Fields;
+using PureQL.CSharp.Model.Parameters;
+using PureQL.CSharp.Model.Returnings;
+using PureQL.CSharp.Model.Scalars;
+using PureQL.CSharp.Model.Serialization.Fields;
+using PureQL.CSharp.Model.Serialization.Parameters;
+using PureQL.CSharp.Model.Serialization.Returnings;
+using PureQL.CSharp.Model.Serialization.Scalars;
+using PureQL.CSharp.Model.Serialization.Types;
+using PureQL.CSharp.Model.Types;
+
+namespace PureQL.CSharp.Model.Serialization.Tests.Returnings;
+
+public sealed record DateReturningConverterTests
+{
+    private readonly JsonSerializerOptions _options = new JsonSerializerOptions()
+    {
+        NewLine = "\n",
+        WriteIndented = true,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        PropertyNameCaseInsensitive = true,
+        Converters =
+        {
+            new DateReturningConverter(),
+            new DateFieldConverter(),
+            new DateParameterConverter(),
+            new DateScalarConverter(),
+            new TypeConverter<DateType>(),
+        },
+    };
+
+    [Fact]
+    public void ReadDateField()
+    {
+        const string expectedEntity = "uheayfodrbniJ";
+        const string expectedField = "ubhjedwasuyhgbefrda";
+        const string input = /*lang=json,strict*/
+            $$"""{"type": {"name":"date"},"entity": "{{expectedEntity}}","field": "{{expectedField}}"}""";
+
+        DateField field = JsonSerializer
+            .Deserialize<DateReturning>(input, _options)!
+            .AsT0;
+
+        Assert.Equal(expectedEntity, field.Entity);
+        Assert.Equal(expectedField, field.Field);
+        Assert.Equal(new DateType(), field.Type);
+    }
+
+    [Fact]
+    public void WriteDateField()
+    {
+        const string expectedEntity = "uheayfodrbniJ";
+        const string expectedField = "ubhjedwasuyhgbefrda";
+
+        string output = JsonSerializer.Serialize(
+            new DateReturning(new DateField(expectedEntity, expectedField)),
+            _options
+        );
+
+        const string expectedOutput = /*lang=json,strict*/
+            $$"""
+            {
+              "entity": "{{expectedEntity}}",
+              "field": "{{expectedField}}",
+              "type": {
+                "name": "date"
+              }
+            }
+            """;
+
+        Assert.Equal(expectedOutput, output);
+    }
+
+    [Fact]
+    public void ReadDateParameter()
+    {
+        const string paramName = "auryehgfbduygbhaerf";
+
+        const string input = /*lang=json,strict*/
+            $$"""{"type": {"name":"date"},"name": "{{paramName}}"}""";
+
+        DateParameter parameter = JsonSerializer
+            .Deserialize<DateReturning>(input, _options)!
+            .AsT1;
+
+        Assert.Equal(paramName, parameter.Name);
+        Assert.Equal(new DateType(), parameter.Type);
+    }
+
+    [Fact]
+    public void WriteDateParameter()
+    {
+        const string expectedParamName = "uheayfodrbniJ";
+
+        string output = JsonSerializer.Serialize(
+            new DateReturning(new DateParameter(expectedParamName)),
+            _options
+        );
+
+        const string expectedOutput = /*lang=json,strict*/
+            $$"""
+            {
+              "name": "{{expectedParamName}}",
+              "type": {
+                "name": "date"
+              }
+            }
+            """;
+
+        Assert.Equal(expectedOutput, output);
+    }
+
+    [Fact]
+    public void ReadDateScalar()
+    {
+        string input = /*lang=json,strict*/
+            $$"""{"type": {"name":"date"},"value": "{{DateTime.Now:yyyy-MM-dd}}"}""";
+
+        DateScalar scalar = JsonSerializer
+            .Deserialize<DateReturning>(input, _options)!
+            .AsT2;
+
+        Assert.Equal(DateOnly.FromDateTime(DateTime.Now), scalar.Value);
+    }
+
+    [Theory]
+    [InlineData("{}")]
+    [InlineData("{asdasdasd}")]
+    [InlineData("""{"asdasd":   }""")]
+    [InlineData(""" """)]
+    public void ThrowsExceptionOnBadFormat(string input)
+    {
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<DateReturning>(input, _options)
+        );
+    }
+
+    [Theory]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"datetime"},"entity": "ufbrdeyhov","field": "heuiyrndfosgv"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"boolean"},"entity": "ufbrdeyhov","field": "heuiyrndfosgv"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"null"},"entity": "ufbrdeyhov","field": "heuiyrndfosgv"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"number"},"entity": "ufbrdeyhov","field": "heuiyrndfosgv"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"string"},"entity": "ufbrdeyhov","field": "heuiyrndfosgv"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"time"},"entity": "ufbrdeyhov","field": "heuiyrndfosgv"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"uuid"},"entity": "ufbrdeyhov","field": "heuiyrndfosgv"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"entity": "ufbrdeyhov","field": "heuiyrndfosgv"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"ihufd"},"entity": "ufbrdeyhov","field": "heuiyrndfosgv"}"""
+    )]
+    public void ThrowsExceptionOnWrongFieldType(string input)
+    {
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<DateReturning>(input, _options)
+        );
+    }
+
+    [Theory]
+    [InlineData( /*lang=json,strict*/
+        """{"type": {"name":"datetime"},"name": "erfinjdhksgt"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"boolean"},"name": "erfinjdhksgt"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"null"},"name": "erfinjdhksgt"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"number"},"name": "erfinjdhksgt"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"string"},"name": "erfinjdhksgt"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"time"},"name": "erfinjdhksgt"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"uuid"},"name": "erfinjdhksgt"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"name": "erfinjdhksgt"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"ihufd"},"name": "erfinjdhksgt"}"""
+    )]
+    public void ThrowsExceptionOnWrongParameterType(string input)
+    {
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<DateReturning>(input, _options)
+        );
+    }
+
+    [Theory]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"datetime"},"value": "2000-01-01"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"boolean"},"value": "2000-01-01"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"null"},"value": "2000-01-01"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"number"},"value": "2000-01-01"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"string"},"value": "2000-01-01"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"time"},"value": "2000-01-01"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"type":{"name":"uuid"},"value": "2000-01-01"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"value": "2000-01-01"}"""
+    )]
+    public void ThrowsExceptionOnWrongScalarType(string input)
+    {
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<DateReturning>(input, _options)
+        );
+    }
+}
