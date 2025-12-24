9ad9ec4989934e59af294afff212642cc7c2ead9
Dmitry Kurochkin <kudima03@gmail.com>
2025-12-24 09:58:15 +0300

    Added null check tests

--- src/Tests/PureQL.CSharp.Model.Serialization.Tests/Fields/TimeFieldConverterTests.cs
+++ src/Tests/PureQL.CSharp.Model.Serialization.Tests/Fields/TimeFieldConverterTests.cs
@@ -56,6 +56,26 @@ public sealed record TimeFieldConverterTests
         Assert.Equal(expected, output);
     }
 
+    [Fact]
+    public void ThrowsExceptionOnMissingEntityField()
+    {
+        const string input = /*lang=json,strict*/
+            """{"field":"jinaudferv","type":{"name":"time"}}""";
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<TimeField>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnMissingFieldField()
+    {
+        const string input = /*lang=json,strict*/
+            """{"entity":"auiheyrdsnf","type":{"name":"time"}}""";
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<TimeField>(input, _options)
+        );
+    }
+
     [Theory]
     [InlineData( /*lang=json,strict*/
         """{"type":{"name":"date"},"entity": "auiheyrdsnf","field": "jinaudferv"}"""
@@ -77,6 +97,9 @@ public sealed record TimeFieldConverterTests
     )]
     [InlineData( /*lang=json,strict*/
         """{"type":{"name":"uuid"},"entity": "auiheyrdsnf","field": "jinaudferv"}"""
+    )]
+    [InlineData( /*lang=json,strict*/
+        """{"entity": "auiheyrdsnf","field": "jinaudferv"}"""
     )]
     public void ThrowsExceptionOnWrongType(string input)
     {
