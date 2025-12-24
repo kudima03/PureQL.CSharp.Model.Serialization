ea876eda9c07d785235db51d62782313d6ab3054
Dmitry Kurochkin <kudima03@gmail.com>
2025-12-24 11:01:09 +0300

    Added null check

--- src/PureQL.CSharp.Model.Serialization/Types/TypeJsonModel.cs
+++ src/PureQL.CSharp.Model.Serialization/Types/TypeJsonModel.cs
@@ -1,10 +1,12 @@
+using System.Text.Json;
+
 namespace PureQL.CSharp.Model.Serialization.Types;
 
 internal sealed record TypeJsonModel
 {
     public TypeJsonModel(string name)
     {
-        Name = name;
+        Name = name ?? throw new JsonException();
     }
 
     public string Name { get; }
