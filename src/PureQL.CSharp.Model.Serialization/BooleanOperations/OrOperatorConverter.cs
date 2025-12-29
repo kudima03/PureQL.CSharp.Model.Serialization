c7a36d03f2c403cd684b00ed8df20ea48736ed27
Dmitry Kurochkin <kudima03@gmail.com>
2025-12-29 11:34:34 +0300

    Implemented OrOperatorConverter

---
+++ src/PureQL.CSharp.Model.Serialization/BooleanOperations/OrOperatorConverter.cs
@@ -0,0 +1,31 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using PureQL.CSharp.Model.BooleanOperations;
+
+namespace PureQL.CSharp.Model.Serialization.BooleanOperations;
+
+public sealed class OrOperatorConverter : JsonConverter<OrOperator>
+{
+    public override OrOperator Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        BooleanOperationJsonModel booleanOperator =
+            JsonSerializer.Deserialize<BooleanOperationJsonModel>(ref reader, options)!;
+
+        return booleanOperator.Operator != BooleanOperator.Or
+            ? throw new JsonException()
+            : new OrOperator(booleanOperator.Conditions);
+    }
+
+    public override void Write(
+        Utf8JsonWriter writer,
+        OrOperator value,
+        JsonSerializerOptions options
+    )
+    {
+        JsonSerializer.Serialize(writer, new BooleanOperationJsonModel(value), options);
+    }
+}
