2662e6c2305d7cb540f61bb5ba58cff7465ce158
Dmitry Kurochkin <kudima03@gmail.com>
2025-12-29 11:29:21 +0300

    Added tests into AndOperatorConverterTests

--- src/Tests/PureQL.CSharp.Model.Serialization.Tests/BooleanOperations/AndOperatorConverterTests.cs
+++ src/Tests/PureQL.CSharp.Model.Serialization.Tests/BooleanOperations/AndOperatorConverterTests.cs
@@ -948,4 +948,301 @@ public sealed record AndOperatorConverterTests
         );
         Assert.Equal(expected, value);
     }
+
+    [Fact(Skip = "NotImplemented")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage(
+        "Usage",
+        "xUnit1004:Test methods should not be skipped",
+        Justification = "<Pending>"
+    )]
+    public void ReadMixedConditions()
+    {
+        const string expectedEntityName = "aruhybfe";
+        const string expectedFieldName = "erafuhyobdng";
+        const string expectedParamName = "ashjlbd";
+
+        string input = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "and",
+              "conditions": [
+                {
+                  "operator": "and",
+                  "conditions": [
+                    {
+                      "type": {
+                        "name": "boolean"
+                      },
+                      "value": false
+                    },
+                    {
+                      "type": {
+                        "name": "boolean"
+                      },
+                      "value": true
+                    }
+                  ]
+                },
+                {
+                  "type": {
+                    "name": "boolean"
+                  },
+                  "value": true
+                },
+                {
+                  "entity": "{{expectedEntityName}}",
+                  "field": "{{expectedFieldName}}",
+                  "type": {
+                    "name": "boolean"
+                  }
+                },
+                {
+                  "name": "{{expectedParamName}}",
+                  "type": {
+                    "name": "boolean"
+                  }
+                },
+                {
+                  "operator": "equal",
+                  "conditions": [
+                    {
+                      "type": {
+                        "name": "boolean"
+                      },
+                      "value": false
+                    },
+                    {
+                      "type": {
+                        "name": "boolean"
+                      },
+                      "value": true
+                    }
+                  ]
+                }
+              ]
+            }
+            """;
+
+        AndOperator value = JsonSerializer.Deserialize<AndOperator>(input, _options)!;
+        Assert.Equal(
+            value.Conditions.First().AsT4,
+            new BooleanOperator(
+                new AndOperator(
+                    [
+                        new BooleanReturning(new BooleanScalar(false)),
+                        new BooleanReturning(new BooleanScalar(true)),
+                    ]
+                )
+            )
+        );
+        Assert.Equal(value.Conditions.Skip(1).First().AsT2, new BooleanScalar(true));
+        Assert.Equal(
+            value.Conditions.Skip(2).First().AsT0,
+            new BooleanField(expectedEntityName, expectedFieldName)
+        );
+        Assert.Equal(
+            value.Conditions.Skip(3).First().AsT1,
+            new BooleanParameter(expectedParamName)
+        );
+        Assert.Equal(
+            value.Conditions.Skip(4).First().AsT3,
+            new Equality(
+                new BooleanEquality(
+                    new BooleanReturning(new BooleanScalar(true)),
+                    new BooleanReturning(new BooleanScalar(false))
+                )
+            )
+        );
+    }
+
+    [Theory(Skip = "NotImplemented")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage(
+        "Usage",
+        "xUnit1004:Test methods should not be skipped",
+        Justification = "<Pending>"
+    )]
+    [InlineData("date")]
+    [InlineData("datetime")]
+    [InlineData("null")]
+    [InlineData("number")]
+    [InlineData("string")]
+    [InlineData("time")]
+    [InlineData("uuid")]
+    public void ThrowsExceptionOnWrongConditionType(string type)
+    {
+        string input = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "and",
+              "conditions": [
+                {
+                  "operator": "and",
+                  "conditions": [
+                    {
+                      "type": {
+                        "name": "{{type}}"
+                      },
+                      "value": false
+                    },
+                    {
+                      "type": {
+                        "name": "{{type}}"
+                      },
+                      "value": true
+                    }
+                  ]
+                },
+                {
+                  "type": {
+                    "name": "{{type}}"
+                  },
+                  "value": true
+                },
+                {
+                  "entity": "arefdjhubn",
+                  "field": "fdevihjn",
+                  "type": {
+                    "name": "{{type}}"
+                  }
+                },
+                {
+                  "name": "swdefiujhnr",
+                  "type": {
+                    "name": "{{type}}"
+                  }
+                },
+                {
+                  "operator": "equal",
+                  "conditions": [
+                    {
+                      "type": {
+                        "name": "{{type}}"
+                      },
+                      "value": false
+                    },
+                    {
+                      "type": {
+                        "name": "{{type}}"
+                      },
+                      "value": true
+                    }
+                  ]
+                }
+              ]
+            }
+            """;
+
+        AndOperator value = JsonSerializer.Deserialize<AndOperator>(input, _options)!;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<AndOperator>(input, _options)
+        );
+    }
+
+    [Fact(Skip = "NotImplemented")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage(
+        "Usage",
+        "xUnit1004:Test methods should not be skipped",
+        Justification = "<Pending>"
+    )]
+    public void WriteMixedConditions()
+    {
+        const string expectedEntityName = "aruhybfe";
+        const string expectedFieldName = "erafuhyobdng";
+        const string expectedParamName = "ashjlbd";
+
+        const string expected = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "and",
+              "conditions": [
+                {
+                  "operator": "and",
+                  "conditions": [
+                    {
+                      "type": {
+                        "name": "boolean"
+                      },
+                      "value": false
+                    },
+                    {
+                      "type": {
+                        "name": "boolean"
+                      },
+                      "value": true
+                    }
+                  ]
+                },
+                {
+                  "type": {
+                    "name": "boolean"
+                  },
+                  "value": true
+                },
+                {
+                  "entity": "{{expectedEntityName}}",
+                  "field": "{{expectedFieldName}}",
+                  "type": {
+                    "name": "boolean"
+                  }
+                },
+                {
+                  "name": "{{expectedParamName}}",
+                  "type": {
+                    "name": "boolean"
+                  }
+                },
+                {
+                  "operator": "equal",
+                  "conditions": [
+                    {
+                      "type": {
+                        "name": "boolean"
+                      },
+                      "value": false
+                    },
+                    {
+                      "type": {
+                        "name": "boolean"
+                      },
+                      "value": true
+                    }
+                  ]
+                }
+              ]
+            }
+            """;
+
+        string value = JsonSerializer.Serialize(
+            new AndOperator(
+                [
+                    new BooleanReturning(
+                        new BooleanOperator(
+                            new AndOperator(
+                                [
+                                    new BooleanReturning(new BooleanScalar(false)),
+                                    new BooleanReturning(new BooleanScalar(true)),
+                                ]
+                            )
+                        )
+                    ),
+                    new BooleanReturning(new BooleanScalar(true)),
+                    new BooleanReturning(
+                        new BooleanField(expectedEntityName, expectedFieldName)
+                    ),
+                    new BooleanReturning(new BooleanParameter(expectedParamName)),
+                    new BooleanReturning(
+                        new Equality(
+                            new BooleanEquality(
+                                new BooleanReturning(new BooleanScalar(true)),
+                                new BooleanReturning(new BooleanScalar(false))
+                            )
+                        )
+                    ),
+                ]
+            ),
+            _options
+        );
+        Assert.Equal(expected, value);
+    }
 }
