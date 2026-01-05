c9814d59083d4edb6eeb6bcba2b56378d023cf65
Dmitry Kurochkin <kudima03@gmail.com>
2026-01-05 15:26:51 +0300

    Tests improved

--- src/Tests/PureQL.CSharp.Model.Serialization.Tests/Equalities/DateTimeEqualityConverterTests.cs
+++ src/Tests/PureQL.CSharp.Model.Serialization.Tests/Equalities/DateTimeEqualityConverterTests.cs
@@ -5,35 +5,27 @@ using PureQL.CSharp.Model.Fields;
 using PureQL.CSharp.Model.Parameters;
 using PureQL.CSharp.Model.Returnings;
 using PureQL.CSharp.Model.Scalars;
-using PureQL.CSharp.Model.Serialization.Equalities;
-using PureQL.CSharp.Model.Serialization.Fields;
-using PureQL.CSharp.Model.Serialization.Parameters;
-using PureQL.CSharp.Model.Serialization.Returnings;
-using PureQL.CSharp.Model.Serialization.Scalars;
-using PureQL.CSharp.Model.Serialization.Types;
-using PureQL.CSharp.Model.Types;
 
 namespace PureQL.CSharp.Model.Serialization.Tests.Equalities;
 
 public sealed record DateTimeEqualityConverterTests
 {
-    private readonly JsonSerializerOptions _options = new JsonSerializerOptions()
+    private readonly JsonSerializerOptions _options;
+
+    public DateTimeEqualityConverterTests()
     {
-        NewLine = "\n",
-        WriteIndented = true,
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-        PropertyNameCaseInsensitive = true,
-        Converters =
+        _options = new JsonSerializerOptions()
         {
-            new JsonStringEnumConverter(JsonNamingPolicy.CamelCase),
-            new DateTimeEqualityConverter(),
-            new DateTimeReturningConverter(),
-            new DateTimeFieldConverter(),
-            new DateTimeParameterConverter(),
-            new DateTimeScalarConverter(),
-            new TypeConverter<DateTimeType>(),
-        },
-    };
+            NewLine = "\n",
+            WriteIndented = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            PropertyNameCaseInsensitive = true,
+        };
+        foreach (JsonConverter converter in new PureQLConverters())
+        {
+            _options.Converters.Add(converter);
+        }
+    }
 
     [Fact]
     public void ThrowsExceptionOnOperatorNameAbsence()
