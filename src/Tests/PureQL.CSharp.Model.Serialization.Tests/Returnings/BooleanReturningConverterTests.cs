a823c46bee687cd7152c1f1fd92ae1d109029f64
Dmitry Kurochkin <kudima03@gmail.com>
2026-01-05 15:28:18 +0300

    Tests improved

--- src/Tests/PureQL.CSharp.Model.Serialization.Tests/Returnings/BooleanReturningConverterTests.cs
+++ src/Tests/PureQL.CSharp.Model.Serialization.Tests/Returnings/BooleanReturningConverterTests.cs
@@ -1,34 +1,31 @@
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using PureQL.CSharp.Model.Fields;
 using PureQL.CSharp.Model.Parameters;
 using PureQL.CSharp.Model.Returnings;
 using PureQL.CSharp.Model.Scalars;
-using PureQL.CSharp.Model.Serialization.Fields;
-using PureQL.CSharp.Model.Serialization.Parameters;
-using PureQL.CSharp.Model.Serialization.Returnings;
-using PureQL.CSharp.Model.Serialization.Scalars;
-using PureQL.CSharp.Model.Serialization.Types;
 using PureQL.CSharp.Model.Types;
 
 namespace PureQL.CSharp.Model.Serialization.Tests.Returnings;
 
 public sealed record BooleanReturningConverterTests
 {
-    private readonly JsonSerializerOptions _options = new JsonSerializerOptions()
+    private readonly JsonSerializerOptions _options;
+
+    public BooleanReturningConverterTests()
     {
-        NewLine = "\n",
-        WriteIndented = true,
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-        PropertyNameCaseInsensitive = true,
-        Converters =
+        _options = new JsonSerializerOptions()
         {
-            new BooleanReturningConverter(),
-            new BooleanFieldConverter(),
-            new BooleanParameterConverter(),
-            new BooleanScalarConverter(),
-            new TypeConverter<BooleanType>(),
-        },
-    };
+            NewLine = "\n",
+            WriteIndented = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            PropertyNameCaseInsensitive = true,
+        };
+        foreach (JsonConverter converter in new PureQLConverters())
+        {
+            _options.Converters.Add(converter);
+        }
+    }
 
     [Fact]
     public void ReadBooleanField()
