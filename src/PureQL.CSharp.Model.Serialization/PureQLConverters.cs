8b585832c83e959f26b53920aedabd34b3639719
Dmitry Kurochkin <kudima03@gmail.com>
2026-01-08 10:58:14 +0300

    StringComparisonConverter added to PureQLConverters

--- src/PureQL.CSharp.Model.Serialization/PureQLConverters.cs
+++ src/PureQL.CSharp.Model.Serialization/PureQLConverters.cs
@@ -64,6 +64,7 @@ public sealed record PureQLConverters : IEnumerable<JsonConverter>
         yield return new DateComparisonConverter();
         yield return new DateTimeComparisonConverter();
         yield return new NumberComparisonConverter();
+        yield return new StringComparisonConverter();
         yield return new TypeConverter<BooleanType>();
         yield return new TypeConverter<DateType>();
         yield return new TypeConverter<DateTimeType>();
