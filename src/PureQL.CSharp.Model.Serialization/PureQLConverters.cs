f715fbee8b0b6ef53dd8fc0d744befe35d445732
kudima03 <kudima03@gmail.com>
2026-01-09 16:50:43 +0300

    ArithmeticConverter addded toPureQLConverters

--- src/PureQL.CSharp.Model.Serialization/PureQLConverters.cs
+++ src/PureQL.CSharp.Model.Serialization/PureQLConverters.cs
@@ -72,6 +72,7 @@ public sealed record PureQLConverters : IEnumerable<JsonConverter>
         yield return new SubtractConverter();
         yield return new MultiplyConverter();
         yield return new DivideConverter();
+        yield return new ArithmeticConverter();
         yield return new TypeConverter<BooleanType>();
         yield return new TypeConverter<DateType>();
         yield return new TypeConverter<DateTimeType>();
