d5dd106023508c38bb0936e3bb3b2aa57c9b9939
kudima03 <kudima03@gmail.com>
2026-01-11 21:33:16 +0300

    Implemented MaxDateConverterTests

---
+++ src/Tests/PureQL.CSharp.Model.Serialization.Tests/Aggregates/Date/MaxDateConverterTests.cs
@@ -0,0 +1,388 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using PureQL.CSharp.Model.Aggregates.Date;
+using PureQL.CSharp.Model.Fields;
+using PureQL.CSharp.Model.Parameters;
+using PureQL.CSharp.Model.Returnings;
+using PureQL.CSharp.Model.Scalars;
+
+namespace PureQL.CSharp.Model.Serialization.Tests.Aggregates.Date;
+
+public sealed record MaxDateConverterTests
+{
+    private readonly JsonSerializerOptions _options;
+
+    public MaxDateConverterTests()
+    {
+        _options = new JsonSerializerOptions()
+        {
+            NewLine = "\n",
+            WriteIndented = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            PropertyNameCaseInsensitive = true,
+        };
+        foreach (JsonConverter converter in new PureQLConverters())
+        {
+            _options.Converters.Add(converter);
+        }
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnOperatorNameAbsence()
+    {
+        const string expectedEntityName = "aruhybfe";
+        const string expectedFieldName = "erafuhyobdng";
+
+        const string input = /*lang=json,strict*/
+            $$"""
+            {
+              "arg": {
+                  "entity": "{{expectedEntityName}}",
+                  "field": "{{expectedFieldName}}",
+                  "type": {
+                    "name": "date"
+                  }
+                }
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MaxDate>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnOtherOperatorName()
+    {
+        const string expectedEntityName = "aruhybfe";
+        const string expectedFieldName = "erafuhyobdng";
+
+        const string input = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "min_date",
+              "arg": {
+                  "entity": "{{expectedEntityName}}",
+                  "field": "{{expectedFieldName}}",
+                  "type": {
+                    "name": "date"
+                  }
+                }
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MaxDate>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnInvalidOperatorName()
+    {
+        const string expectedEntityName = "aruhybfe";
+        const string expectedFieldName = "erafuhyobdng";
+
+        const string input = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "min_date",
+              "arg": {
+                  "entity": "{{expectedEntityName}}",
+                  "field": "{{expectedFieldName}}",
+                  "type": {
+                    "name": "date"
+                  }
+                }
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MaxDate>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnUndefinedArgument()
+    {
+        const string input = /*lang=json,strict*/
+            """
+            {
+              "operator": "max_date",
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MaxDate>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnNullArgument()
+    {
+        const string input = /*lang=json,strict*/
+            """
+            {
+              "operator": "max_date",
+              "arg": null
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MaxDate>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnArgumentWrongType()
+    {
+        const string input = /*lang=json,strict*/
+            """
+            {
+              "operator": "max_date",
+              "arg": []
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MaxDate>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void ReadScalarArgument()
+    {
+        DateOnly now = DateOnly.FromDateTime(DateTime.Now);
+        string input = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "max_date",
+              "arg": {
+                  "type": {
+                    "name": "date"
+                  },
+                  "value": {{JsonSerializer.Serialize(now, _options)}}
+                }
+            }
+            """;
+
+        MaxDate value = JsonSerializer.Deserialize<MaxDate>(input, _options)!;
+        Assert.Equal(new DateScalar(now), value.Argument.AsT2);
+    }
+
+    [Theory]
+    [InlineData("date")]
+    [InlineData("datetime")]
+    [InlineData("null")]
+    [InlineData("number")]
+    [InlineData("string")]
+    [InlineData("time")]
+    [InlineData("uuid")]
+    public void ThrowsExceptionOnWrongScalarType(string type)
+    {
+        DateOnly now = DateOnly.FromDateTime(DateTime.Now);
+        string input = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "max_date",
+              "arg": {
+                  "type": {
+                    "name": "{{type}}"
+                  },
+                  "value": {{JsonSerializer.Serialize(now, _options)}}"
+                }
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MaxDate>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void WriteScalarArgument()
+    {
+        DateOnly now = DateOnly.FromDateTime(DateTime.Now);
+        string expected = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "max_date",
+              "arg": {
+                "type": {
+                  "name": "date"
+                },
+                "value": {{JsonSerializer.Serialize(now, _options)}}
+              }
+            }
+            """;
+
+        string value = JsonSerializer.Serialize(
+            new MaxDate(new DateReturning(new DateScalar(now))),
+            _options
+        );
+        Assert.Equal(expected, value);
+    }
+
+    [Fact]
+    public void ReadParameterArgument()
+    {
+        const string expectedParamName = "ashjlbd";
+
+        const string input = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "max_date",
+              "arg": {
+                  "name": "{{expectedParamName}}",
+                  "type": {
+                    "name": "date"
+                  }
+                }
+            }
+            """;
+
+        MaxDate value = JsonSerializer.Deserialize<MaxDate>(input, _options)!;
+        Assert.Equal(value.Argument.AsT1, new DateParameter(expectedParamName));
+    }
+
+    [Theory]
+    [InlineData("date")]
+    [InlineData("datetime")]
+    [InlineData("null")]
+    [InlineData("number")]
+    [InlineData("string")]
+    [InlineData("time")]
+    [InlineData("uuid")]
+    public void ThrowsExceptionOnWrongParameterType(string type)
+    {
+        const string expectedParamName = "ashjlbd";
+
+        string input = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "not",
+              "arg": {
+                  "name": "{{expectedParamName}}",
+                  "type": {
+                    "name": "{{type}}"
+                  }
+                }
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MaxDate>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void WriteParameterArgument()
+    {
+        const string expectedParamName = "ashjlbd";
+
+        const string expected = $$"""
+            {
+              "operator": "max_date",
+              "arg": {
+                "name": "{{expectedParamName}}",
+                "type": {
+                  "name": "date"
+                }
+              }
+            }
+            """;
+
+        string value = JsonSerializer.Serialize(
+            new MaxDate(new DateReturning(new DateParameter(expectedParamName))),
+            _options
+        );
+        Assert.Equal(expected, value);
+    }
+
+    [Fact]
+    public void ReadFieldArgument()
+    {
+        const string expectedEntityName = "aruhybfe";
+        const string expectedFieldName = "erafuhyobdng";
+
+        const string input = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "max_date",
+              "arg": {
+                  "entity": "{{expectedEntityName}}",
+                  "field": "{{expectedFieldName}}",
+                  "type": {
+                    "name": "date"
+                  }
+                }
+            }
+            """;
+
+        MaxDate value = JsonSerializer.Deserialize<MaxDate>(input, _options)!;
+        Assert.Equal(
+            value.Argument.AsT0,
+            new DateField(expectedEntityName, expectedFieldName)
+        );
+    }
+
+    [Theory]
+    [InlineData("date")]
+    [InlineData("datetime")]
+    [InlineData("null")]
+    [InlineData("number")]
+    [InlineData("string")]
+    [InlineData("time")]
+    [InlineData("uuid")]
+    public void ThrowsExceptionOnWrongFieldType(string type)
+    {
+        const string expectedEntityName = "aruhybfe";
+        const string expectedFieldName = "erafuhyobdng";
+
+        string input = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "max_date",
+              "arg": {
+                  "entity": "{{expectedEntityName}}"
+                  "field": "{{expectedFieldName}}",
+                  "type": {
+                    "name": "{{type}}"
+                  }
+                }
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MaxDate>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void WriteFieldArgument()
+    {
+        const string expectedEntityName = "aruhybfe";
+        const string expectedFieldName = "erafuhyobdng";
+
+        const string expected = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "max_date",
+              "arg": {
+                "entity": "{{expectedEntityName}}",
+                "field": "{{expectedFieldName}}",
+                "type": {
+                  "name": "date"
+                }
+              }
+            }
+            """;
+
+        string value = JsonSerializer.Serialize(
+            new MaxDate(
+                new DateReturning(new DateField(expectedEntityName, expectedFieldName))
+            ),
+            _options
+        );
+        Assert.Equal(expected, value);
+    }
+}
