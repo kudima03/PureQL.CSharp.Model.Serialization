dd447999fa00f518a14ecbdfbc21004283c08f94
Dmitry Kurochkin <kudima03@gmail.com>
2026-01-12 11:25:54 +0300

    Implemented MinNumberConverterTests

---
+++ src/Tests/PureQL.CSharp.Model.Serialization.Tests/Aggregates/Numeric/MinNumberConverterTests.cs
@@ -0,0 +1,395 @@
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using PureQL.CSharp.Model.Aggregates.Numeric;
+using PureQL.CSharp.Model.Fields;
+using PureQL.CSharp.Model.Parameters;
+using PureQL.CSharp.Model.Returnings;
+using PureQL.CSharp.Model.Scalars;
+
+namespace PureQL.CSharp.Model.Serialization.Tests.Aggregates.Numeric;
+
+public sealed record MinNumberConverterTests
+{
+    private readonly JsonSerializerOptions _options;
+
+    public MinNumberConverterTests()
+    {
+        _options = new JsonSerializerOptions()
+        {
+            NewLine = "\n",
+            WriteIndented = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            PropertyNameCaseInsensitive = true,
+        };
+        foreach (JsonConverter converter in new PureQLConverters())
+        {
+            _options.Converters.Add(converter);
+        }
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnOperatorNameAbsence()
+    {
+        const string expectedEntityName = "aruhybfe";
+        const string expectedFieldName = "erafuhyobdng";
+
+        const string input = /*lang=json,strict*/
+            $$"""
+            {
+              "arg": {
+                  "entity": "{{expectedEntityName}}",
+                  "field": "{{expectedFieldName}}",
+                  "type": {
+                    "name": "number"
+                  }
+                }
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MinNumber>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnOtherOperatorName()
+    {
+        const string expectedEntityName = "aruhybfe";
+        const string expectedFieldName = "erafuhyobdng";
+
+        const string input = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "average_number",
+              "arg": {
+                  "entity": "{{expectedEntityName}}",
+                  "field": "{{expectedFieldName}}",
+                  "type": {
+                    "name": "number"
+                  }
+                }
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MinNumber>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnInvalidOperatorName()
+    {
+        const string expectedEntityName = "aruhybfe";
+        const string expectedFieldName = "erafuhyobdng";
+
+        const string input = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "euhwyrfdbuyeghrfdb",
+              "arg": {
+                  "entity": "{{expectedEntityName}}",
+                  "field": "{{expectedFieldName}}",
+                  "type": {
+                    "name": "number"
+                  }
+                }
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MinNumber>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnUndefinedArgument()
+    {
+        const string input = /*lang=json,strict*/
+            """
+            {
+              "operator": "min_number"
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MinNumber>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnNullArgument()
+    {
+        const string input = /*lang=json,strict*/
+            """
+            {
+              "operator": "min_number",
+              "arg": null
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MinNumber>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnArgumentWrongType()
+    {
+        const string input = /*lang=json,strict*/
+            """
+            {
+              "operator": "min_number",
+              "arg": []
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MinNumber>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void ReadScalarArgument()
+    {
+        double number = Random.Shared.NextDouble();
+        string input = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "min_number",
+              "arg": {
+                  "type": {
+                    "name": "number"
+                  },
+                  "value": {{number.ToString(CultureInfo.InvariantCulture)}}
+                }
+            }
+            """;
+
+        MinNumber value = JsonSerializer.Deserialize<MinNumber>(input, _options)!;
+        Assert.Equal(new NumberScalar(number), value.Argument.AsT2);
+    }
+
+    [Theory]
+    [InlineData("boolean")]
+    [InlineData("date")]
+    [InlineData("null")]
+    [InlineData("datetime")]
+    [InlineData("string")]
+    [InlineData("time")]
+    [InlineData("uuid")]
+    [InlineData("refhyuabogs")]
+    public void ThrowsExceptionOnWrongScalarType(string type)
+    {
+        DateOnly number = DateOnly.FromDateTime(DateTime.Now);
+        string input = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "min_number",
+              "arg": {
+                  "type": {
+                    "name": "{{type}}"
+                  },
+                  "value": {{number.ToString(CultureInfo.InvariantCulture)}}
+                }
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MinNumber>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void WriteScalarArgument()
+    {
+        double number = Random.Shared.NextDouble();
+        string expected = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "min_number",
+              "arg": {
+                "type": {
+                  "name": "number"
+                },
+                "value": {{number.ToString(CultureInfo.InvariantCulture)}}
+              }
+            }
+            """;
+
+        string value = JsonSerializer.Serialize(
+            new MinNumber(new NumberReturning(new NumberScalar(number))),
+            _options
+        );
+        Assert.Equal(expected, value);
+    }
+
+    [Fact]
+    public void ReadParameterArgument()
+    {
+        const string expectedParamName = "ashjlbd";
+
+        const string input = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "min_number",
+              "arg": {
+                  "name": "{{expectedParamName}}",
+                  "type": {
+                    "name": "number"
+                  }
+                }
+            }
+            """;
+
+        MinNumber value = JsonSerializer.Deserialize<MinNumber>(input, _options)!;
+        Assert.Equal(new NumberParameter(expectedParamName), value.Argument.AsT1);
+    }
+
+    [Theory]
+    [InlineData("boolean")]
+    [InlineData("date")]
+    [InlineData("null")]
+    [InlineData("datetime")]
+    [InlineData("string")]
+    [InlineData("time")]
+    [InlineData("uuid")]
+    [InlineData("ehufry")]
+    public void ThrowsExceptionOnWrongParameterType(string type)
+    {
+        const string expectedParamName = "ashjlbd";
+
+        string input = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "min_number",
+              "arg": {
+                  "name": "{{expectedParamName}}",
+                  "type": {
+                    "name": "{{type}}"
+                  }
+                }
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MinNumber>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void WriteParameterArgument()
+    {
+        const string expectedParamName = "ashjlbd";
+
+        const string expected = $$"""
+            {
+              "operator": "min_number",
+              "arg": {
+                "name": "{{expectedParamName}}",
+                "type": {
+                  "name": "number"
+                }
+              }
+            }
+            """;
+
+        string value = JsonSerializer.Serialize(
+            new MinNumber(
+                new NumberReturning(new NumberParameter(expectedParamName))
+            ),
+            _options
+        );
+        Assert.Equal(expected, value);
+    }
+
+    [Fact]
+    public void ReadFieldArgument()
+    {
+        const string expectedEntityName = "aruhybfe";
+        const string expectedFieldName = "erafuhyobdng";
+
+        const string input = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "min_number",
+              "arg": {
+                  "entity": "{{expectedEntityName}}",
+                  "field": "{{expectedFieldName}}",
+                  "type": {
+                    "name": "number"
+                  }
+                }
+            }
+            """;
+
+        MinNumber value = JsonSerializer.Deserialize<MinNumber>(input, _options)!;
+        Assert.Equal(
+            new NumberField(expectedEntityName, expectedFieldName),
+            value.Argument.AsT0
+        );
+    }
+
+    [Theory]
+    [InlineData("boolean")]
+    [InlineData("date")]
+    [InlineData("null")]
+    [InlineData("datetime")]
+    [InlineData("string")]
+    [InlineData("time")]
+    [InlineData("uuid")]
+    public void ThrowsExceptionOnWrongFieldType(string type)
+    {
+        const string expectedEntityName = "aruhybfe";
+        const string expectedFieldName = "erafuhyobdng";
+
+        string input = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "min_number",
+              "arg": {
+                  "entity": "{{expectedEntityName}}",
+                  "field": "{{expectedFieldName}}",
+                  "type": {
+                    "name": "{{type}}"
+                  }
+                }
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MinNumber>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void WriteFieldArgument()
+    {
+        const string expectedEntityName = "aruhybfe";
+        const string expectedFieldName = "erafuhyobdng";
+
+        const string expected = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "min_number",
+              "arg": {
+                "entity": "{{expectedEntityName}}",
+                "field": "{{expectedFieldName}}",
+                "type": {
+                  "name": "number"
+                }
+              }
+            }
+            """;
+
+        string value = JsonSerializer.Serialize(
+            new MinNumber(
+                new NumberReturning(
+                    new NumberField(expectedEntityName, expectedFieldName)
+                )
+            ),
+            _options
+        );
+        Assert.Equal(expected, value);
+    }
+}
