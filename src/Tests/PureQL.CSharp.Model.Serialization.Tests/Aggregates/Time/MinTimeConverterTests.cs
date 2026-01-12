17bb51125d9cc12802b24f68eceb29fe9280db15
Dmitry Kurochkin <kudima03@gmail.com>
2026-01-12 15:57:38 +0300

    Implemented MinTimeConverterTests

---
+++ src/Tests/PureQL.CSharp.Model.Serialization.Tests/Aggregates/Time/MinTimeConverterTests.cs
@@ -0,0 +1,390 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using PureQL.CSharp.Model.Aggregates.Time;
+using PureQL.CSharp.Model.Fields;
+using PureQL.CSharp.Model.Parameters;
+using PureQL.CSharp.Model.Returnings;
+using PureQL.CSharp.Model.Scalars;
+
+namespace PureQL.CSharp.Model.Serialization.Tests.Aggregates.Time;
+
+public sealed record MinTimeConverterTests
+{
+    private readonly JsonSerializerOptions _options;
+
+    public MinTimeConverterTests()
+    {
+        _options = new JsonSerializerOptions()
+        {
+            NewLine = "\n",
+            WriteIndented = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            PropertyNameCaseInsensitive = true,
+        };
+        foreach (JsonConverter converter in new PureQLConverters())
+        {
+            _options.Converters.Add(converter);
+        }
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnOperatorNameAbsence()
+    {
+        const string expectedEntityName = "aruhybfe";
+        const string expectedFieldName = "erafuhyobdng";
+
+        const string input = /*lang=json,strict*/
+            $$"""
+            {
+              "arg": {
+                  "entity": "{{expectedEntityName}}",
+                  "field": "{{expectedFieldName}}",
+                  "type": {
+                    "name": "time"
+                  }
+                }
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MinTime>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnOtherOperatorName()
+    {
+        const string expectedEntityName = "aruhybfe";
+        const string expectedFieldName = "erafuhyobdng";
+
+        const string input = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "max_date",
+              "arg": {
+                  "entity": "{{expectedEntityName}}",
+                  "field": "{{expectedFieldName}}",
+                  "type": {
+                    "name": "time"
+                  }
+                }
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MinTime>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnInvalidOperatorName()
+    {
+        const string expectedEntityName = "aruhybfe";
+        const string expectedFieldName = "erafuhyobdng";
+
+        const string input = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "euhwyrfdbuyeghrfdb",
+              "arg": {
+                  "entity": "{{expectedEntityName}}",
+                  "field": "{{expectedFieldName}}",
+                  "type": {
+                    "name": "time"
+                  }
+                }
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MinTime>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnUndefinedArgument()
+    {
+        const string input = /*lang=json,strict*/
+            """
+            {
+              "operator": "max_date"
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MinTime>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnNullArgument()
+    {
+        const string input = /*lang=json,strict*/
+            """
+            {
+              "operator": "min_time",
+              "arg": null
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MinTime>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnArgumentWrongType()
+    {
+        const string input = /*lang=json,strict*/
+            """
+            {
+              "operator": "min_time",
+              "arg": []
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MinTime>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void ReadScalarArgument()
+    {
+        TimeOnly now = TimeOnly.FromDateTime(DateTime.Now);
+        string input = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "min_time",
+              "arg": {
+                  "type": {
+                    "name": "time"
+                  },
+                  "value": {{JsonSerializer.Serialize(now, _options)}}
+                }
+            }
+            """;
+
+        MinTime value = JsonSerializer.Deserialize<MinTime>(input, _options)!;
+        Assert.Equal(new TimeScalar(now), value.Argument.AsT2);
+    }
+
+    [Theory]
+    [InlineData("boolean")]
+    [InlineData("datetime")]
+    [InlineData("null")]
+    [InlineData("number")]
+    [InlineData("string")]
+    [InlineData("date")]
+    [InlineData("uuid")]
+    [InlineData("refhyuabogs")]
+    public void ThrowsExceptionOnWrongScalarType(string type)
+    {
+        TimeOnly now = TimeOnly.FromDateTime(DateTime.Now);
+        string input = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "min_time",
+              "arg": {
+                  "type": {
+                    "name": "{{type}}"
+                  },
+                  "value": {{JsonSerializer.Serialize(now, _options)}}
+                }
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MinTime>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void WriteScalarArgument()
+    {
+        TimeOnly now = TimeOnly.FromDateTime(DateTime.Now);
+        string expected = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "min_time",
+              "arg": {
+                "type": {
+                  "name": "time"
+                },
+                "value": {{JsonSerializer.Serialize(now, _options)}}
+              }
+            }
+            """;
+
+        string value = JsonSerializer.Serialize(
+            new MinTime(new TimeReturning(new TimeScalar(now))),
+            _options
+        );
+        Assert.Equal(expected, value);
+    }
+
+    [Fact]
+    public void ReadParameterArgument()
+    {
+        const string expectedParamName = "ashjlbd";
+
+        const string input = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "min_time",
+              "arg": {
+                  "name": "{{expectedParamName}}",
+                  "type": {
+                    "name": "time"
+                  }
+                }
+            }
+            """;
+
+        MinTime value = JsonSerializer.Deserialize<MinTime>(input, _options)!;
+        Assert.Equal(new TimeParameter(expectedParamName), value.Argument.AsT1);
+    }
+
+    [Theory]
+    [InlineData("boolean")]
+    [InlineData("datetime")]
+    [InlineData("null")]
+    [InlineData("number")]
+    [InlineData("string")]
+    [InlineData("date")]
+    [InlineData("uuid")]
+    [InlineData("ehufry")]
+    public void ThrowsExceptionOnWrongParameterType(string type)
+    {
+        const string expectedParamName = "ashjlbd";
+
+        string input = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "min_time",
+              "arg": {
+                  "name": "{{expectedParamName}}",
+                  "type": {
+                    "name": "{{type}}"
+                  }
+                }
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MinTime>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void WriteParameterArgument()
+    {
+        const string expectedParamName = "ashjlbd";
+
+        const string expected = $$"""
+            {
+              "operator": "min_time",
+              "arg": {
+                "name": "{{expectedParamName}}",
+                "type": {
+                  "name": "time"
+                }
+              }
+            }
+            """;
+
+        string value = JsonSerializer.Serialize(
+            new MinTime(new TimeReturning(new TimeParameter(expectedParamName))),
+            _options
+        );
+        Assert.Equal(expected, value);
+    }
+
+    [Fact]
+    public void ReadFieldArgument()
+    {
+        const string expectedEntityName = "aruhybfe";
+        const string expectedFieldName = "erafuhyobdng";
+
+        const string input = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "min_time",
+              "arg": {
+                  "entity": "{{expectedEntityName}}",
+                  "field": "{{expectedFieldName}}",
+                  "type": {
+                    "name": "time"
+                  }
+                }
+            }
+            """;
+
+        MinTime value = JsonSerializer.Deserialize<MinTime>(input, _options)!;
+        Assert.Equal(
+            new TimeField(expectedEntityName, expectedFieldName),
+            value.Argument.AsT0
+        );
+    }
+
+    [Theory]
+    [InlineData("boolean")]
+    [InlineData("datetime")]
+    [InlineData("null")]
+    [InlineData("number")]
+    [InlineData("string")]
+    [InlineData("date")]
+    [InlineData("uuid")]
+    public void ThrowsExceptionOnWrongFieldType(string type)
+    {
+        const string expectedEntityName = "aruhybfe";
+        const string expectedFieldName = "erafuhyobdng";
+
+        string input = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "min_time",
+              "arg": {
+                  "entity": "{{expectedEntityName}}",
+                  "field": "{{expectedFieldName}}",
+                  "type": {
+                    "name": "{{type}}"
+                  }
+                }
+            }
+            """;
+
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<MinTime>(input, _options)
+        );
+    }
+
+    [Fact]
+    public void WriteFieldArgument()
+    {
+        const string expectedEntityName = "aruhybfe";
+        const string expectedFieldName = "erafuhyobdng";
+
+        const string expected = /*lang=json,strict*/
+            $$"""
+            {
+              "operator": "min_time",
+              "arg": {
+                "entity": "{{expectedEntityName}}",
+                "field": "{{expectedFieldName}}",
+                "type": {
+                  "name": "time"
+                }
+              }
+            }
+            """;
+
+        string value = JsonSerializer.Serialize(
+            new MinTime(
+                new TimeReturning(new TimeField(expectedEntityName, expectedFieldName))
+            ),
+            _options
+        );
+        Assert.Equal(expected, value);
+    }
+}
