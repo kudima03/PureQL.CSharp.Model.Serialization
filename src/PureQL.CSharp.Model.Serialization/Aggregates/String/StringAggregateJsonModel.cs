20a56a67093a073878512839027f1d2013d85eeb
Dmitry Kurochkin <kudima03@gmail.com>
2026-01-12 11:56:52 +0300

    Implemented StringAggregateJsonModel

---
+++ src/PureQL.CSharp.Model.Serialization/Aggregates/String/StringAggregateJsonModel.cs
@@ -0,0 +1,36 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using PureQL.CSharp.Model.Aggregates.String;
+using PureQL.CSharp.Model.Returnings;
+
+namespace PureQL.CSharp.Model.Serialization.Aggregates.String;
+
+internal enum StringAggregateOperatorJsonModel
+{
+    None,
+    min_string,
+    max_string,
+}
+
+internal sealed record StringAggregateJsonModel
+{
+    public StringAggregateJsonModel(MinString minString)
+        : this(StringAggregateOperatorJsonModel.min_string, minString.Argument) { }
+
+    public StringAggregateJsonModel(MaxString maxString)
+        : this(StringAggregateOperatorJsonModel.max_string, maxString.Argument) { }
+
+    [JsonConstructor]
+    public StringAggregateJsonModel(
+        StringAggregateOperatorJsonModel @operator,
+        StringReturning arg
+    )
+    {
+        Operator = @operator;
+        Arg = arg ?? throw new JsonException();
+    }
+
+    public StringAggregateOperatorJsonModel Operator { get; }
+
+    public StringReturning Arg { get; }
+}
