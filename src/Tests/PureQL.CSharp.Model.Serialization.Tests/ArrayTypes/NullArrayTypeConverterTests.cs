93d41498e80712a035ce638fb3974349517743c5
Dmitry Kurochkin <kudima03@gmail.com>
2026-01-27 14:40:16 +0300

    Implemented NullArrayTypeConverterTests

---
+++ src/Tests/PureQL.CSharp.Model.Serialization.Tests/ArrayTypes/NullArrayTypeConverterTests.cs
@@ -0,0 +1,76 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using PureQL.CSharp.Model.ArrayTypes;
+
+namespace PureQL.CSharp.Model.Serialization.Tests.ArrayTypes;
+
+public sealed record NullArrayTypeConverterTests
+{
+    private readonly JsonSerializerOptions _options;
+
+    public NullArrayTypeConverterTests()
+    {
+        _options = new JsonSerializerOptions()
+        {
+            NewLine = "\n",
+            WriteIndented = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            PropertyNameCaseInsensitive = true,
+        };
+        foreach (JsonConverter converter in new PureQLConverters())
+        {
+            _options.Converters.Add(converter);
+        }
+    }
+
+    [Fact]
+    public void Read()
+    {
+        const string input = /*lang=json,strict*/
+            """
+            {
+              "name": "nullArray"
+            }
+            """;
+
+        NullArrayType type = JsonSerializer.Deserialize<NullArrayType>(input, _options)!;
+
+        Assert.Equal(type.Name, new NullArrayType().Name);
+    }
+
+    [Fact]
+    public void Write()
+    {
+        string output = JsonSerializer.Serialize(new NullArrayType(), _options);
+
+        Assert.Equal( /*lang=json,strict*/
+            """
+            {
+              "name": "nullArray"
+            }
+            """,
+            output
+        );
+    }
+
+    [Theory]
+    [InlineData("uuidArray")]
+    [InlineData("booleanArray")]
+    [InlineData("dateArray")]
+    [InlineData("numberArray")]
+    [InlineData("datetimeArray")]
+    [InlineData("stringArray")]
+    [InlineData("timeArray")]
+    [InlineData("")]
+    public void ThrowsExceptionOnWrongType(string type)
+    {
+        string input = $$"""
+            {
+              "name": "{{type}}"
+            }
+            """;
+        _ = Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<NullArrayType>(input, _options)
+        );
+    }
+}
