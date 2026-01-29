1942ca8bb7b85f1dab5d62a2a5b873dbb1e9e62b
Dmitry Kurochkin <kudima03@gmail.com>
2026-01-29 15:50:36 +0300

    SumNumberConverterTests updated

--- src/Tests/PureQL.CSharp.Model.Serialization.Tests/Aggregates/Numeric/SumNumberConverterTests.cs
+++ src/Tests/PureQL.CSharp.Model.Serialization.Tests/Aggregates/Numeric/SumNumberConverterTests.cs
@@ -1,11 +1,10 @@
-using System.Globalization;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using PureQL.CSharp.Model.Aggregates.Numeric;
+using PureQL.CSharp.Model.ArrayParameters;
+using PureQL.CSharp.Model.ArrayReturnings;
+using PureQL.CSharp.Model.ArrayScalars;
 using PureQL.CSharp.Model.Fields;
-using PureQL.CSharp.Model.Parameters;
-using PureQL.CSharp.Model.Returnings;
-using PureQL.CSharp.Model.Scalars;
 
 namespace PureQL.CSharp.Model.Serialization.Tests.Aggregates.Numeric;
 
@@ -41,7 +40,7 @@ public sealed record SumNumberConverterTests
                   "entity": "{{expectedEntityName}}",
                   "field": "{{expectedFieldName}}",
                   "type": {
-                    "name": "number"
+                    "name": "numberArray"
                   }
                 }
             }
@@ -66,7 +65,7 @@ public sealed record SumNumberConverterTests
                   "entity": "{{expectedEntityName}}",
                   "field": "{{expectedFieldName}}",
                   "type": {
-                    "name": "number"
+                    "name": "numberArray"
                   }
                 }
             }
@@ -91,7 +90,7 @@ public sealed record SumNumberConverterTests
                   "entity": "{{expectedEntityName}}",
                   "field": "{{expectedFieldName}}",
                   "type": {
-                    "name": "number"
+                    "name": "numberArray"
                   }
                 }
             }
@@ -152,22 +151,32 @@ public sealed record SumNumberConverterTests
     [Fact]
     public void ReadScalarArgument()
     {
-        double number = Random.Shared.NextDouble();
+        IEnumerable<double> values =
+        [
+            Random.Shared.NextDouble(),
+            Random.Shared.NextDouble(),
+            Random.Shared.NextDouble(),
+        ];
+
         string input = /*lang=json,strict*/
             $$"""
             {
               "operator": "sum",
               "arg": {
-                  "type": {
-                    "name": "number"
-                  },
-                  "value": {{number.ToString(CultureInfo.InvariantCulture)}}
-                }
+                "type": {
+                  "name": "numberArray"
+                },
+                "value": [
+                  {{values.First()}},
+                  {{values.Skip(1).First()}},
+                  {{values.Skip(2).First()}}
+                ]
+              }
             }
             """;
 
         SumNumber value = JsonSerializer.Deserialize<SumNumber>(input, _options)!;
-        Assert.Equal(new NumberScalar(number), value.Argument.AsT2);
+        Assert.Equal(new NumberArrayScalar(values), value.Argument.AsT2);
     }
 
     [Theory]
@@ -178,20 +187,37 @@ public sealed record SumNumberConverterTests
     [InlineData("string")]
     [InlineData("time")]
     [InlineData("uuid")]
+    [InlineData("booleanArray")]
+    [InlineData("dateArray")]
+    [InlineData("nullArray")]
+    [InlineData("datetimeArray")]
+    [InlineData("stringArray")]
+    [InlineData("timeArray")]
+    [InlineData("uuidArray")]
     [InlineData("refhyuabogs")]
     public void ThrowsExceptionOnWrongScalarType(string type)
     {
-        DateOnly number = DateOnly.FromDateTime(DateTime.Now);
+        IEnumerable<double> values =
+        [
+            Random.Shared.NextDouble(),
+            Random.Shared.NextDouble(),
+            Random.Shared.NextDouble(),
+        ];
+
         string input = /*lang=json,strict*/
             $$"""
             {
               "operator": "sum",
               "arg": {
-                  "type": {
-                    "name": "{{type}}"
-                  },
-                  "value": {{number.ToString(CultureInfo.InvariantCulture)}}
-                }
+                "type": {
+                  "name": "{{type}}"
+                },
+                "value": [
+                  {{values.First()}},
+                  {{values.Skip(1).First()}},
+                  {{values.Skip(2).First()}}
+                ]
+              }
             }
             """;
 
@@ -203,22 +229,32 @@ public sealed record SumNumberConverterTests
     [Fact]
     public void WriteScalarArgument()
     {
-        double number = Random.Shared.NextDouble();
+        IEnumerable<double> values =
+        [
+            Random.Shared.NextDouble(),
+            Random.Shared.NextDouble(),
+            Random.Shared.NextDouble(),
+        ];
+
         string expected = /*lang=json,strict*/
             $$"""
             {
               "operator": "sum",
               "arg": {
                 "type": {
-                  "name": "number"
+                  "name": "numberArray"
                 },
-                "value": {{number.ToString(CultureInfo.InvariantCulture)}}
+                "value": [
+                  {{values.First()}},
+                  {{values.Skip(1).First()}},
+                  {{values.Skip(2).First()}}
+                ]
               }
             }
             """;
 
         string value = JsonSerializer.Serialize(
-            new SumNumber(new NumberReturning(new NumberScalar(number))),
+            new SumNumber(new NumberArrayReturning(new NumberArrayScalar(values))),
             _options
         );
         Assert.Equal(expected, value);
@@ -236,14 +272,14 @@ public sealed record SumNumberConverterTests
               "arg": {
                   "name": "{{expectedParamName}}",
                   "type": {
-                    "name": "number"
+                    "name": "numberArray"
                   }
                 }
             }
             """;
 
         SumNumber value = JsonSerializer.Deserialize<SumNumber>(input, _options)!;
-        Assert.Equal(new NumberParameter(expectedParamName), value.Argument.AsT1);
+        Assert.Equal(new NumberArrayParameter(expectedParamName), value.Argument.AsT0);
     }
 
     [Theory]
@@ -254,7 +290,14 @@ public sealed record SumNumberConverterTests
     [InlineData("string")]
     [InlineData("time")]
     [InlineData("uuid")]
-    [InlineData("ehufry")]
+    [InlineData("booleanArray")]
+    [InlineData("dateArray")]
+    [InlineData("nullArray")]
+    [InlineData("datetimeArray")]
+    [InlineData("stringArray")]
+    [InlineData("timeArray")]
+    [InlineData("uuidArray")]
+    [InlineData("refhyuabogs")]
     public void ThrowsExceptionOnWrongParameterType(string type)
     {
         const string expectedParamName = "ashjlbd";
@@ -288,14 +331,16 @@ public sealed record SumNumberConverterTests
               "arg": {
                 "name": "{{expectedParamName}}",
                 "type": {
-                  "name": "number"
+                  "name": "numberArray"
                 }
               }
             }
             """;
 
         string value = JsonSerializer.Serialize(
-            new SumNumber(new NumberReturning(new NumberParameter(expectedParamName))),
+            new SumNumber(
+                new NumberArrayReturning(new NumberArrayParameter(expectedParamName))
+            ),
             _options
         );
         Assert.Equal(expected, value);
@@ -315,7 +360,7 @@ public sealed record SumNumberConverterTests
                   "entity": "{{expectedEntityName}}",
                   "field": "{{expectedFieldName}}",
                   "type": {
-                    "name": "number"
+                    "name": "numberArray"
                   }
                 }
             }
@@ -324,7 +369,7 @@ public sealed record SumNumberConverterTests
         SumNumber value = JsonSerializer.Deserialize<SumNumber>(input, _options)!;
         Assert.Equal(
             new NumberField(expectedEntityName, expectedFieldName),
-            value.Argument.AsT0
+            value.Argument.AsT1
         );
     }
 
@@ -336,6 +381,14 @@ public sealed record SumNumberConverterTests
     [InlineData("string")]
     [InlineData("time")]
     [InlineData("uuid")]
+    [InlineData("booleanArray")]
+    [InlineData("dateArray")]
+    [InlineData("nullArray")]
+    [InlineData("datetimeArray")]
+    [InlineData("stringArray")]
+    [InlineData("timeArray")]
+    [InlineData("uuidArray")]
+    [InlineData("refhyuabogs")]
     public void ThrowsExceptionOnWrongFieldType(string type)
     {
         const string expectedEntityName = "aruhybfe";
@@ -374,7 +427,7 @@ public sealed record SumNumberConverterTests
                 "entity": "{{expectedEntityName}}",
                 "field": "{{expectedFieldName}}",
                 "type": {
-                  "name": "number"
+                  "name": "numberArray"
                 }
               }
             }
@@ -382,7 +435,7 @@ public sealed record SumNumberConverterTests
 
         string value = JsonSerializer.Serialize(
             new SumNumber(
-                new NumberReturning(
+                new NumberArrayReturning(
                     new NumberField(expectedEntityName, expectedFieldName)
                 )
             ),
