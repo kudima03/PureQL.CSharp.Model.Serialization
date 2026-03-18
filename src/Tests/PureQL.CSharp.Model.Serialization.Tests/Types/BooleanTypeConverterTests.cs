5924851fb40dc7b4e027d47629e0517ebc5f2438
kudima03 <kudima03@gmail.com>
2026-03-18 21:44:55 +0300

    Style fixes in BooleanTypeConverterTests

--- src/Tests/PureQL.CSharp.Model.Serialization.Tests/Types/BooleanTypeConverterTests.cs
+++ src/Tests/PureQL.CSharp.Model.Serialization.Tests/Types/BooleanTypeConverterTests.cs
@@ -41,16 +41,16 @@ public sealed record BooleanTypeConverterTests
     [Fact]
     public void Write()
     {
-        string output = JsonSerializer.Serialize(new BooleanType(), _options);
-
-        Assert.Equal( /*lang=json,strict*/
+        const string expected = /*lang=json,strict*/
             """
             {
               "name": "boolean"
             }
-            """,
-            output
-        );
+            """;
+
+        string output = JsonSerializer.Serialize(new BooleanType(), _options);
+
+        Assert.Equal(expected, output);
     }
 
     [Theory]
