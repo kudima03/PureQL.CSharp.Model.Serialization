682384f2dca71605a9917344c28779e4abaa9820
kudima03 <kudima03@gmail.com>
2026-03-19 22:04:57 +0300

    Use single hash-based assertion in MaxTimeConverterTests

--- src/Tests/PureQL.CSharp.Model.Serialization.Tests/Aggregates/Time/MaxTimeConverterTests.cs
+++ src/Tests/PureQL.CSharp.Model.Serialization.Tests/Aggregates/Time/MaxTimeConverterTests.cs
@@ -179,8 +179,7 @@ public sealed record MaxTimeConverterTests
             }
             """;
 
-        MaxTime value = JsonSerializer.Deserialize<MaxTime>(input, _options)!;
-        Assert.Equal(expectedValues, value.Argument.AsT2.Value);
+        Assert.Equal(expectedValues, JsonSerializer.Deserialize<MaxTime>(input, _options)!.Argument.AsT2.Value);
     }
 
     [Theory]
@@ -290,8 +289,7 @@ public sealed record MaxTimeConverterTests
             }
             """;
 
-        MaxTime value = JsonSerializer.Deserialize<MaxTime>(input, _options)!;
-        Assert.Equal(new TimeArrayParameter(expectedParamName), value.Argument.AsT0);
+        Assert.Equal(new TimeArrayParameter(expectedParamName), JsonSerializer.Deserialize<MaxTime>(input, _options)!.Argument.AsT0);
     }
 
     [Theory]
@@ -378,11 +376,7 @@ public sealed record MaxTimeConverterTests
             }
             """;
 
-        MaxTime value = JsonSerializer.Deserialize<MaxTime>(input, _options)!;
-        Assert.Equal(
-            new TimeField(expectedEntityName, expectedFieldName),
-            value.Argument.AsT1
-        );
+        Assert.Equal(new TimeField(expectedEntityName, expectedFieldName), JsonSerializer.Deserialize<MaxTime>(input, _options)!.Argument.AsT1);
     }
 
     [Theory]
