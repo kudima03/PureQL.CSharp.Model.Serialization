824488d7a664aa780a11f8da70f35d887225a33a
kudima03 <kudima03@gmail.com>
2026-03-19 22:04:25 +0300

    Use single hash-based assertion in SingleValueEqualityConverterTests

--- src/Tests/PureQL.CSharp.Model.Serialization.Tests/Equalities/SingleValueEqualityConverterTests.cs
+++ src/Tests/PureQL.CSharp.Model.Serialization.Tests/Equalities/SingleValueEqualityConverterTests.cs
@@ -50,12 +50,15 @@ public sealed record SingleValueEqualityConverterTests
             }
             """;
 
-        BooleanEquality equality = JsonSerializer
-            .Deserialize<SingleValueEquality>(input, _options)!
-            .AsT0;
-
-        Assert.Equal(new BooleanScalar(true), equality.Left.AsT1);
-        Assert.Equal(new BooleanScalar(false), equality.Right.AsT1);
+        Assert.Equal(
+            new SingleValueEquality(
+                new BooleanEquality(
+                    new BooleanReturning(new BooleanScalar(true)),
+                    new BooleanReturning(new BooleanScalar(false))
+                )
+            ),
+            JsonSerializer.Deserialize<SingleValueEquality>(input, _options)
+        );
     }
 
     [Fact]
@@ -118,12 +121,15 @@ public sealed record SingleValueEqualityConverterTests
             }
             """;
 
-        BooleanEquality equality = JsonSerializer
-            .Deserialize<SingleValueEquality>(input, _options)!
-            .AsT0;
-
-        Assert.Equal(new BooleanParameter(leftParamName), equality.Left.AsT0);
-        Assert.Equal(new BooleanParameter(rightParamName), equality.Right.AsT0);
+        Assert.Equal(
+            new SingleValueEquality(
+                new BooleanEquality(
+                    new BooleanReturning(new BooleanParameter(leftParamName)),
+                    new BooleanReturning(new BooleanParameter(rightParamName))
+                )
+            ),
+            JsonSerializer.Deserialize<SingleValueEquality>(input, _options)
+        );
     }
 
     [Fact]
@@ -204,12 +210,24 @@ public sealed record SingleValueEqualityConverterTests
             }
             """;
 
-        BooleanEquality equality = JsonSerializer
-            .Deserialize<SingleValueEquality>(input, _options)!
-            .AsT0;
+        BooleanEquality innerEquality = new(
+            new BooleanReturning(new BooleanScalar(true)),
+            new BooleanReturning(new BooleanScalar(false))
+        );
 
-        Assert.Equal(new BooleanScalar(true), equality.Left.AsT2.AsT0.AsT0.Left.AsT1);
-        Assert.Equal(new BooleanScalar(false), equality.Left.AsT2.AsT0.AsT0.Right.AsT1);
+        Assert.Equal(
+            new SingleValueEquality(
+                new BooleanEquality(
+                    new BooleanReturning(
+                        new Equality(new SingleValueEquality(innerEquality))
+                    ),
+                    new BooleanReturning(
+                        new Equality(new SingleValueEquality(innerEquality))
+                    )
+                )
+            ),
+            JsonSerializer.Deserialize<SingleValueEquality>(input, _options)
+        );
     }
 
     [Fact]
@@ -424,14 +442,31 @@ public sealed record SingleValueEqualityConverterTests
             }
             """;
 
-        BooleanEquality equality = JsonSerializer
-            .Deserialize<SingleValueEquality>(input, _options)!
-            .AsT0;
-
-        Assert.Equal(ComparisonOperator.GreaterThan, equality.Left.AsT4.AsT2.Operator);
-        Assert.Equal(new NumberScalar(42), equality.Left.AsT4.AsT2.Left.AsT1);
-        Assert.Equal(ComparisonOperator.LessThan, equality.Right.AsT4.AsT2.Operator);
-        Assert.Equal(new NumberScalar(10), equality.Right.AsT4.AsT2.Left.AsT1);
+        Assert.Equal(
+            new SingleValueEquality(
+                new BooleanEquality(
+                    new BooleanReturning(
+                        new Comparison(
+                            new NumberComparison(
+                                ComparisonOperator.GreaterThan,
+                                new NumberReturning(new NumberScalar(42)),
+                                new NumberReturning(new NumberScalar(24))
+                            )
+                        )
+                    ),
+                    new BooleanReturning(
+                        new Comparison(
+                            new NumberComparison(
+                                ComparisonOperator.LessThan,
+                                new NumberReturning(new NumberScalar(10)),
+                                new NumberReturning(new NumberScalar(20))
+                            )
+                        )
+                    )
+                )
+            ),
+            JsonSerializer.Deserialize<SingleValueEquality>(input, _options)
+        );
     }
 
     [Fact]
@@ -528,12 +563,15 @@ public sealed record SingleValueEqualityConverterTests
             }
             """;
 
-        DateEquality equality = JsonSerializer
-            .Deserialize<SingleValueEquality>(input, _options)!
-            .AsT1;
-
-        Assert.Equal(new DateParameter(leftParamName), equality.Left.AsT0);
-        Assert.Equal(new DateParameter(rightParamName), equality.Right.AsT0);
+        Assert.Equal(
+            new SingleValueEquality(
+                new DateEquality(
+                    new DateReturning(new DateParameter(leftParamName)),
+                    new DateReturning(new DateParameter(rightParamName))
+                )
+            ),
+            JsonSerializer.Deserialize<SingleValueEquality>(input, _options)
+        );
     }
 
     [Fact]
@@ -599,12 +637,15 @@ public sealed record SingleValueEqualityConverterTests
             }
             """;
 
-        DateEquality equality = JsonSerializer
-            .Deserialize<SingleValueEquality>(input, _options)!
-            .AsT1;
-
-        Assert.Equal(new DateScalar(leftValue), equality.Left.AsT1);
-        Assert.Equal(new DateScalar(rightValue), equality.Right.AsT1);
+        Assert.Equal(
+            new SingleValueEquality(
+                new DateEquality(
+                    new DateReturning(new DateScalar(leftValue)),
+                    new DateReturning(new DateScalar(rightValue))
+                )
+            ),
+            JsonSerializer.Deserialize<SingleValueEquality>(input, _options)
+        );
     }
 
     [Fact]
@@ -670,12 +711,15 @@ public sealed record SingleValueEqualityConverterTests
             }
             """;
 
-        DateTimeEquality equality = JsonSerializer
-            .Deserialize<SingleValueEquality>(input, _options)!
-            .AsT2;
-
-        Assert.Equal(new DateTimeParameter(leftParamName), equality.Left.AsT0);
-        Assert.Equal(new DateTimeParameter(rightParamName), equality.Right.AsT0);
+        Assert.Equal(
+            new SingleValueEquality(
+                new DateTimeEquality(
+                    new DateTimeReturning(new DateTimeParameter(leftParamName)),
+                    new DateTimeReturning(new DateTimeParameter(rightParamName))
+                )
+            ),
+            JsonSerializer.Deserialize<SingleValueEquality>(input, _options)
+        );
     }
 
     [Fact]
@@ -744,12 +788,15 @@ public sealed record SingleValueEqualityConverterTests
             }
             """;
 
-        DateTimeEquality equality = JsonSerializer
-            .Deserialize<SingleValueEquality>(input, _options)!
-            .AsT2;
-
-        Assert.Equal(new DateTimeScalar(leftValue), equality.Left.AsT1);
-        Assert.Equal(new DateTimeScalar(rightValue), equality.Right.AsT1);
+        Assert.Equal(
+            new SingleValueEquality(
+                new DateTimeEquality(
+                    new DateTimeReturning(new DateTimeScalar(leftValue)),
+                    new DateTimeReturning(new DateTimeScalar(rightValue))
+                )
+            ),
+            JsonSerializer.Deserialize<SingleValueEquality>(input, _options)
+        );
     }
 
     [Fact]
@@ -815,12 +862,15 @@ public sealed record SingleValueEqualityConverterTests
             }
             """;
 
-        NumberEquality equality = JsonSerializer
-            .Deserialize<SingleValueEquality>(input, _options)!
-            .AsT3;
-
-        Assert.Equal(new NumberScalar(42), equality.Left.AsT1);
-        Assert.Equal(new NumberScalar(24), equality.Right.AsT1);
+        Assert.Equal(
+            new SingleValueEquality(
+                new NumberEquality(
+                    new NumberReturning(new NumberScalar(42)),
+                    new NumberReturning(new NumberScalar(24))
+                )
+            ),
+            JsonSerializer.Deserialize<SingleValueEquality>(input, _options)
+        );
     }
 
     [Fact]
@@ -883,12 +933,15 @@ public sealed record SingleValueEqualityConverterTests
             }
             """;
 
-        NumberEquality equality = JsonSerializer
-            .Deserialize<SingleValueEquality>(input, _options)!
-            .AsT3;
-
-        Assert.Equal(new NumberParameter(leftParamName), equality.Left.AsT0);
-        Assert.Equal(new NumberParameter(rightParamName), equality.Right.AsT0);
+        Assert.Equal(
+            new SingleValueEquality(
+                new NumberEquality(
+                    new NumberReturning(new NumberParameter(leftParamName)),
+                    new NumberReturning(new NumberParameter(rightParamName))
+                )
+            ),
+            JsonSerializer.Deserialize<SingleValueEquality>(input, _options)
+        );
     }
 
     [Fact]
@@ -954,12 +1007,15 @@ public sealed record SingleValueEqualityConverterTests
             }
             """;
 
-        StringEquality equality = JsonSerializer
-            .Deserialize<SingleValueEquality>(input, _options)!
-            .AsT4;
-
-        Assert.Equal(new StringScalar(leftValue), equality.Left.AsT1);
-        Assert.Equal(new StringScalar(rightValue), equality.Right.AsT1);
+        Assert.Equal(
+            new SingleValueEquality(
+                new StringEquality(
+                    new StringReturning(new StringScalar(leftValue)),
+                    new StringReturning(new StringScalar(rightValue))
+                )
+            ),
+            JsonSerializer.Deserialize<SingleValueEquality>(input, _options)
+        );
     }
 
     [Fact]
@@ -1025,12 +1081,15 @@ public sealed record SingleValueEqualityConverterTests
             }
             """;
 
-        StringEquality equality = JsonSerializer
-            .Deserialize<SingleValueEquality>(input, _options)!
-            .AsT4;
-
-        Assert.Equal(new StringParameter(leftParamName), equality.Left.AsT0);
-        Assert.Equal(new StringParameter(rightParamName), equality.Right.AsT0);
+        Assert.Equal(
+            new SingleValueEquality(
+                new StringEquality(
+                    new StringReturning(new StringParameter(leftParamName)),
+                    new StringReturning(new StringParameter(rightParamName))
+                )
+            ),
+            JsonSerializer.Deserialize<SingleValueEquality>(input, _options)
+        );
     }
 
     [Fact]
@@ -1096,12 +1155,15 @@ public sealed record SingleValueEqualityConverterTests
             }
             """;
 
-        TimeEquality equality = JsonSerializer
-            .Deserialize<SingleValueEquality>(input, _options)!
-            .AsT5;
-
-        Assert.Equal(new TimeParameter(leftParamName), equality.Left.AsT0);
-        Assert.Equal(new TimeParameter(rightParamName), equality.Right.AsT0);
+        Assert.Equal(
+            new SingleValueEquality(
+                new TimeEquality(
+                    new TimeReturning(new TimeParameter(leftParamName)),
+                    new TimeReturning(new TimeParameter(rightParamName))
+                )
+            ),
+            JsonSerializer.Deserialize<SingleValueEquality>(input, _options)
+        );
     }
 
     [Fact]
@@ -1167,12 +1229,15 @@ public sealed record SingleValueEqualityConverterTests
             }
             """;
 
-        TimeEquality equality = JsonSerializer
-            .Deserialize<SingleValueEquality>(input, _options)!
-            .AsT5;
-
-        Assert.Equal(new TimeScalar(leftValue), equality.Left.AsT1);
-        Assert.Equal(new TimeScalar(rightValue), equality.Right.AsT1);
+        Assert.Equal(
+            new SingleValueEquality(
+                new TimeEquality(
+                    new TimeReturning(new TimeScalar(leftValue)),
+                    new TimeReturning(new TimeScalar(rightValue))
+                )
+            ),
+            JsonSerializer.Deserialize<SingleValueEquality>(input, _options)
+        );
     }
 
     [Fact]
@@ -1238,12 +1303,15 @@ public sealed record SingleValueEqualityConverterTests
             }
             """;
 
-        UuidEquality equality = JsonSerializer
-            .Deserialize<SingleValueEquality>(input, _options)!
-            .AsT6;
-
-        Assert.Equal(new UuidScalar(leftValue), equality.Left.AsT1);
-        Assert.Equal(new UuidScalar(rightValue), equality.Right.AsT1);
+        Assert.Equal(
+            new SingleValueEquality(
+                new UuidEquality(
+                    new UuidReturning(new UuidScalar(leftValue)),
+                    new UuidReturning(new UuidScalar(rightValue))
+                )
+            ),
+            JsonSerializer.Deserialize<SingleValueEquality>(input, _options)
+        );
     }
 
     [Fact]
@@ -1309,12 +1377,15 @@ public sealed record SingleValueEqualityConverterTests
             }
             """;
 
-        UuidEquality equality = JsonSerializer
-            .Deserialize<SingleValueEquality>(input, _options)!
-            .AsT6;
-
-        Assert.Equal(new UuidParameter(leftParamName), equality.Left.AsT0);
-        Assert.Equal(new UuidParameter(rightParamName), equality.Right.AsT0);
+        Assert.Equal(
+            new SingleValueEquality(
+                new UuidEquality(
+                    new UuidReturning(new UuidParameter(leftParamName)),
+                    new UuidReturning(new UuidParameter(rightParamName))
+                )
+            ),
+            JsonSerializer.Deserialize<SingleValueEquality>(input, _options)
+        );
     }
 
     [Fact]
